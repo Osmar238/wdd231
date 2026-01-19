38597ba4ddca32a2d55418967eb86718deb0462b
Osmar238 <osmar77770@gmail.com>
2026-01-19 17:58:17 -0600

    .

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,4 +1,3 @@
-// 1. Lógica del Menú Hamburguesa
 const hamButton = document.querySelector('#menu-toggle');
 const navigation = document.querySelector('nav');
 
@@ -7,12 +6,9 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
-// 2. Lógica del Footer (Año y Última Modificación)
-// Obtener el año actual
 const yearSpan = document.querySelector("#year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
 
-// Obtener la última modificación del documento
 const lastModifiedSpan = document.querySelector("#lastModified");
 lastModifiedSpan.textContent = `Last Modification: ${document.lastModified}`;
